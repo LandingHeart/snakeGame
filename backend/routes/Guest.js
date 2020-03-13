700fbec4d0ad51d6954b48cd8d78b4d0c1ada630
LandingHeart <land.liu0123@gmail.com>
2020-03-12 23:08:22 -0400

    routes change

--- backend/routes/Guest.js
+++ backend/routes/Guest.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Guest = require("../models/Guest");
 
-router.get("/guest", async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const guest = await Guest.find();
     res.json(guest);
--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,8 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
-const router = express.Router();
+const guestModel = require("./routes/guest");
 const port = process.env.PORT || 8080;
-
 
 require("dotenv/config");
 
@@ -15,17 +14,11 @@ mongoose.connect(
   { useNewUrlParser: true, useUnifiedTopology: true },
   () => console.log("connected to port " + port)
 );
+app.use("/guest", guestModel);
 app.use(bodyParser.json());
 app.use(cookieparser());
 app.use(cors());
 
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("/build"));
-  app.use("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
-  });
-}
 app.listen(port, () => {
   console.log(`Server running at PORT: ${port}`);
 });
