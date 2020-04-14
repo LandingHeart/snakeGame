1c2fa76834244cd690d28bb8f89b29eed94cf902
LandingHeart <land.liu0123@gmail.com>
2020-04-14 15:50:52 -0400

    untracked node_modules

--- index.js
+++ index.js
@@ -34,6 +34,9 @@ app.use("/score", scoreModel);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("frontend/build"));
 }
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+});
 
 app.listen(port, () => {
   console.log(`Server running at PORT: ${port}`);
